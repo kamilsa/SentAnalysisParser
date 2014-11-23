134087b00e6fc9cdb718cbe4446a58474c4711a2
kamilsa <kamilsa16@gmail.com>
2014-11-23 13:19:25 +0300

    change a little bit structure of the table in db

--- src/ru/kamil/innopolis/sentiment/parser/TheGuardianPageParser.java
+++ src/ru/kamil/innopolis/sentiment/parser/TheGuardianPageParser.java
@@ -11,7 +11,7 @@ import java.util.HashSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.jsoup .Jsoup;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
